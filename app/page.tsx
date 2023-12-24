0337212135188d13d23ed97200ec28a3f7839f4b
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-12-25 08:35:13 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,15 @@ const Home = async () => {
   return (
     <div>
       <h1 className="text-2xl font-bold">Hello</h1>
-      <ul>
+      <ul className="space-y-2">
         {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.title} {formatDate(todo.createdAt, 'YYYY-MM-DD HH:mm:ss')}{' '}
-            {formatDate(todo.updatedAt, 'YYYY-MM-DD HH:mm:ss')}
+          <li className="flex flex-col" key={todo.id}>
+            <span className="text-xs text-gray-400">{todo.id}</span>
+            <span>{todo.title}</span>
+            <span>
+              {formatDate(todo.createdAt, 'YYYY-MM-DD HH:mm:ss')}
+              {formatDate(todo.updatedAt, 'YYYY-MM-DD HH:mm:ss')}
+            </span>
           </li>
         ))}
       </ul>
