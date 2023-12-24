6ea81c850f23e387881ba97def704863fffbaea8
Sanshiroh SAKAI <sansyo@gmail.com>
2023-12-24 20:48:30 +0900

    環境構築

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,35 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      aspectRatio: {
+        gold: '1.618 / 1',
+        goldReverse: '1 / 1.618',
+        silver: '1.4 / 1',
+        silverReverse: '1 / 1.4',
+      },
+      fontFamily: {
+        body: [
+          'var(--font-inter)',
+          'Hiragino Sans',
+          'Hiragino Kaku Gothic Pro',
+          'sans-serif',
+        ],
+      },
+      fontSize: {
+        '3xl': '2rem',
+        '4xl': '2.5rem',
+      },
+      lineHeight: {
+        inherit: 'inherit',
+        relaxed: '1.8',
+      },
+      maxWidth: {
+        '7xl': '75rem',
       },
     },
   },
-  plugins: [],
 }
 export default config
